90cd3ff2be6c9a6241bc5a2f2a47bc8564019f49
Indro Lie <Indrolie@gmail.com>
2018-08-01 14:32:26 +0700

    add color to level-0

---
+++ level-0/index.js
@@ -0,0 +1,11 @@
+console.log("JAVASCRIPT");
+const colors = require("colors");
+
+var a = 4;
+let b = 5;
+const c = 7;
+
+const result = a + b + c;
+
+console.log(`4 + 5 + 7 = ${result}`.rainbow);
+console.log("Hello World!".rainbow);
